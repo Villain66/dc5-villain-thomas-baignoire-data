ae29f6b8432bcfbc9061b7b706c5df94f8ec7d72
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-11-06 11:19:56 +0100

    add exo2a + gestion d'erreur

--- ex2-a.py
+++ ex2-a.py
@@ -1,6 +1,8 @@
 liste_depenses_marketing = [100, 200, 350, 550]
+if liste_depenses_marketing == []:
+    print(0)
+else:
+    for depense in liste_depenses_marketing:
+        liste_depenses_marketing =+ depense
 
-for depense in liste_depenses_marketing:
-    liste_depenses_marketing =+ depense
-
-print(liste_depenses_marketing)
+    print(liste_depenses_marketing)
