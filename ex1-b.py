5df319ac3c32ba7c20fc66cac9bdd8d0edf0dab8
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-11-06 15:39:21 +0100

    correctif : prise en compte des caractères spéciaux

--- ex1-b.py
+++ ex1-b.py
@@ -24,6 +24,26 @@ chaine_originale = input("Tapez une phrase : ")
 chaine_convertie_majuscule = convertir_en_majuscule(chaine_originale)
 chaine_convertie_minuscule = convertir_en_minuscule(chaine_originale)
 
+nombre_mots = 0
+precedent_alphanum = False
+precedent_ponctuation = False
+
+for caractere in chaine_originale:
+    if ('a' <= caractere <= 'z') or ('A' <= caractere <= 'Z') or ('0' <= caractere <= '9') or caractere in "àâäéèêëïîôöùûüç":
+        if not precedent_alphanum:
+            nombre_mots += 1
+        precedent_alphanum = True
+        precedent_ponctuation = False 
+    else:
+        if caractere in "'-!.?,":
+            if not precedent_ponctuation:
+                nombre_mots += 1
+            precedent_ponctuation = True
+        else:
+            precedent_ponctuation = False
+        precedent_alphanum = False
+
+
 print(f"Phrase en majuscules : {''.join(chaine_convertie_majuscule)}")
 print(f"Phrase en minuscules : {''.join(chaine_convertie_minuscule)}")
-print(f"Nombre de caractères : {len(chaine_originale)}")
+print(f"Nombre de mots dans la phrase : {nombre_mots}")
