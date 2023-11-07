6a70c19e2d335a0533530773d36a5c7b8aece959
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-11-07 21:34:17 +0100

    add exo5 + fonction génération d'emails

---
+++ ex5.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generation_email():
+    caracteres = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(caracteres) for _ in range(7)) + "@gmail.com"
+
+clients = [
+    {
+        "nom": f"Client{i}",
+        "email": generation_email(),
+        "montant_depense": round(random.uniform(100.0, 900.0), 2)
+    }
+    for i in range(1, 51)  # Permet de générer 50 entrées
+]
+
+# Permet d'afficher le dicitionnaire :
+for client in clients:
+    print(f"Nom: {client['nom']}, Email: {client['email']}, Montant dépensé: {client['montant_depense']}")
